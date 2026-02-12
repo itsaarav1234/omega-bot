d37c0f4289db0b167f850c5ca571408d3ad73477
itsaarav1234 <aaravbajpai571@gmail.com>
2026-02-12 14:50:38 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const app = express();
+app.get("/", (req, res) => res.send("Omega Bot is Alive 👑"));
+app.listen(process.env.PORT || 3000);
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("Pong 👑");
+  }
+});
+
+client.login(process.env.TOKEN);
