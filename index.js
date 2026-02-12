6ac0a5e3d8b50dee42100eecae122e87425be6d9
itsaarav1234 <aaravbajpai571@gmail.com>
2026-02-12 20:54:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Web server running on port ${PORT}`);
 });
+
 require("dotenv").config();
 
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
